31e2b3d2e32da301ddc873a9e5add1e364411fcc
chan <114630069+cc-StringBuffer-123@users.noreply.github.com>
2024-07-26 16:38:03 +0800

    Add files via upload

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b5c28f23-c87e-49f3-9670-3333503e8ea3</testSuiteGuid>
+</TestSuiteEntity>
